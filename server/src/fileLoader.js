66c65284ef1f9e9e5a650c1f9083e98394efcaca
SilverFox70 <wfbrinkert@gmail.com>
2017-04-03 13:34:30 -0700

    wip: find .on calls and parse out the eventName and callback name

--- server/src/fileLoader.js
+++ server/src/fileLoader.js
@@ -2,7 +2,7 @@ var fileLoader = function(){
 	'use-strict';
   const fs = require('fs');
 
-  var fileList = function(path){
+  var getFileList = function(path){
     return new Promise(function(resolve, reject){
       var jsFiles = [];
       fs.readdir(path, function(err, files){
@@ -21,10 +21,13 @@ var fileLoader = function(){
 
   var readFile = function(file){
     return new Promise(function(resolve, reject){
-      fs.readFile(__dirname + '/' + file, function(err, data){
+      fs.readFile(__dirname + '/' + file, function(err, rawFileData){
         if (err) {
           reject(err);
         }
+        var data = {};
+        data.fileName = file;
+        data.fileText = rawFileData.toString();
         resolve(data);
       });
     });
@@ -56,40 +59,41 @@ var fileLoader = function(){
   // };
 
   return {
-    fileList,
     createFilesObject,
-    getFileData : function(){
-      return new Promise(function(resolve, reject){
-        this.fileList(__dirname).then(function(filelist){
-          createFilesObject(filelist).then(function(rawfiles){
-            resolve(rawfiles);
-          }).catch(function(err){
-            console.error(err);
-            reject(err);
-          });
-        }).catch(function(err){
-          console.error(err);
-          reject(err);
-        });
-      });
-    }
+    getFileList,
+    readFile
   }
 
 }();
 
-var allfiles = fileLoader.getFileData().then(function(allfiles){
-  console.log(allfiles.toString());
-})
+var findEventAndCallback = function(line, index){
+  var openP = line.indexOf('(', index);
+  var closeP = line.indexOf(')', openP);
+  var substr = line.substring(openP +1, closeP);
+  var rawEventandCB = [];
+  substr.split(',').forEach(function(part){
+    rawEventandCB.push(part.trim().replace(/(\'|\"")/g, ''));
+  });
+  console.log('eventName: ' + rawEventandCB[0] + '\tcallback: ' + rawEventandCB[1]);
+}
 
-
-// fileLoader.fileList(__dirname).then(function(files){
-//   files.forEach(function(file){
-//     console.log(file);
-//   });
-//   fileLoader.createFilesObject(files).then(function(rawfiles){
-//     rawfiles.forEach(function(rawfile){
-//       console.log(rawfile);
-//     });
-//   });
-// });
+fileLoader.getFileList(__dirname).then(function(files){
+  files.forEach(function(file){
+    console.log(file);
+  });
+  fileLoader.createFilesObject(files).then(function(rawfiles){
+    rawfiles.forEach(function(rawfile){
+      console.log('-----------------------------------------------------');
+      console.log(rawfile.fileName);
+      var lines = rawfile.fileText.split('\n');
+      for(var i = 0; i < lines.length ; i++){
+        var index = lines[i].indexOf('.on');
+        if ( index !== -1){
+          findEventAndCallback(lines[i], index);
+        }
+        console.log( (i+1) + ' : ' + lines[i]);
+      }
+    });
+  });
+});
 
