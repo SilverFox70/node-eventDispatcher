f8f5c84f7d7b7e2d54b173ccc1a504dfe72fc241
SilverFox70 <wfbrinkert@gmail.com>
2017-04-20 16:19:05 -0700

    wip: set export; fill out logic for creating json object of subscribed and emitted events from all files in server/src

--- server/src/fileLoader.js
+++ server/src/fileLoader.js
@@ -66,21 +66,40 @@ var fileLoader = function(){
 
 }();
 
+module.exports = { fileLoader: fileLoader};
+
 var clc = require('cli-color');
 
 var yellow = clc.yellow;
 var cyan = clc.cyan;
+var mag = clc.magenta;
 
-var findOnEventNameAndCallback = function(line, index){
+var findOnEventNameAndCallback = function(line, index, lineNumber, doOnce){
   var openP = line.indexOf('(', index);
   var closeP = line.indexOf(')', openP);
   var substr = line.substring(openP +1, closeP);
   var rawEventandCB = [];
+
   substr.split(',').forEach(function(part){
     rawEventandCB.push(part.trim().replace(/(\'|\"")/g, ''));
   });
+
   console.log(yellow('eventName: ') + cyan(rawEventandCB[0]) + yellow('\tcallback: ') + cyan(rawEventandCB[1]));
-  return { eventName: rawEventandCB[0], callback: rawEventandCB[1]};
+  return { eventName: rawEventandCB[0], callback: rawEventandCB[1], once: doOnce,line: lineNumber };
+}
+
+var findEmitEventsandArgs = function(line, index, lineNumber){
+  var openP = line.indexOf('(', index);
+  var closeP = line.indexOf(')', openP);
+  var substr = line.substring(openP +1, closeP);
+  var rawEmitandData = [];
+
+  substr.split(',').forEach(function(part){
+    rawEmitandData.push(part.trim().replace(/(\'|\"")/g, ''));
+  });
+
+  console.log(mag('eventName: ') + cyan(rawEmitandData[0]) + mag('\tdata: ') + cyan(rawEmitandData[1]));
+  return { eventName: rawEmitandData[0], data: rawEmitandData[1], line: lineNumber };
 }
 
 fileLoader.getFileList(__dirname).then(function(files){
@@ -90,22 +109,34 @@ fileLoader.getFileList(__dirname).then(function(files){
   fileLoader.createFilesObject(files).then(function(rawfiles){
     rawfiles.forEach(function(rawfile){
       console.log('-----------------------------------------------------');
-      console.log(rawfile.fileName);
+      console.log(clc.bold(rawfile.fileName));
 
       var fileEventMap = {};
       fileEventMap.file = rawfile.fileName;
       fileEventMap.subscribedEvents = [];
+      fileEventMap.invokedEvents = [];
 
       var lines = rawfile.fileText.split('\n');
       for(var i = 0; i < lines.length ; i++){
-        var index = lines[i].indexOf('.on');
 
+        var index = lines[i].indexOf('.on');
         if ( index !== -1){
-           fileEventMap.subscribedEvents.push(findOnEventNameAndCallback(lines[i], index));
+          // if this is not a case of 'once', log as 'on'
+          if (lines[i].indexOf('.once') === -1){
+            fileEventMap.subscribedEvents.push(findOnEventNameAndCallback(lines[i], index, i+1, false));
+          } else {
+            fileEventMap.subscribedEvents.push(findOnEventNameAndCallback(lines[i], index3, i+1, true));
+          }
+
         }
 
-        console.log( (i+1) + ' : ' + lines[i]);
-      }
+        var index2 = lines[i].indexOf('.emit');
+        if (index2 !== -1){
+          fileEventMap.invokedEvents.push(findEmitEventsandArgs(lines[i], index2, i+1));
+        }
+
+        console.log( clc.blackBright((i+1) + ' : ') + lines[i]);
+      } // End for...loop
 
       console.log(clc.blue(JSON.stringify(fileEventMap, null, '  ')));
     });
